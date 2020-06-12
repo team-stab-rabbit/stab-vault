e493cee71cfd7ffc385ecaabfd8c29d3b7e5b6f0
Robert Wise <robcodehub@gmail.com>
2020-06-12 12:42:18 +1000

    setting up config files and linting

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
-const color = require('color');
+const express = require('express')
+const app = express()
+const color = require('color')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.status(200).send('works');
-});
+  res.status(200).send('works')
+})
 
 app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
+  console.log(`Server running at port ${PORT}`)
+})
