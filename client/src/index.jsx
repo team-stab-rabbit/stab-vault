4750a2acb782d0cb258bb894c23a6f5c19362991
Robert Luo <robertxluo@gmail.com>
2020-06-25 21:50:37 -0500

    Add css reset

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/app.component';
-import './global.style.css'
+import 'minireset.css';
+import './global.style.css';
 
 render(<App />, document.querySelector('#app'));
