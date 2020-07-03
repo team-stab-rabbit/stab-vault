a5edfd2c6c9be7cbf9b0c51a7d851ec0a9057dee
Tai N <ndhuutai1@gmail.com>
2020-07-02 18:30:34 -0700

    added a simple get route to userpath

--- server/routes/api/userpaths.js
+++ server/routes/api/userpaths.js
@@ -5,35 +5,35 @@ const express = require('express');
 const router = express.Router();
 const UserPath = require('../../models/userpath.js');
 
-router.post('/',
-  async (req, res) => {
-    try {
-      const {
-        name,
-        author,
-        description,
-        tags,
-        likes,
-        collections,
-        mainPath,
-        completed,
-      } = req.body;
-      const userPathDetails = {};
-      if (name) userPathDetails.name = name;
-      if (author) userPathDetails.author = author;
-      if (description) userPathDetails.description = description;
-      if (tags) userPathDetails.tags = tags;
-      if (likes) userPathDetails.likes = likes;
-      if (collections) userPathDetails.collections = collections;
-      if (mainPath) userPathDetails.mainPath = mainPath;
-      if (completed) userPathDetails.mainPath = completed;
-      const userPath = new UserPath(userPathDetails);
-      await userPath.save();
-      res.json(userPath);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(`Server error: ${err._message}`);
-    }
-  });
+router.get('/', async (req, res) => {
+  try {
+    const allpaths = await UserPath.find({}).exec();
+    res.json(allpaths);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(`Server error: ${e._message}`);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, author, description, tags, likes, collections, mainPath, completed } = req.body;
+    const userPathDetails = {};
+    if (name) userPathDetails.name = name;
+    if (author) userPathDetails.author = author;
+    if (description) userPathDetails.description = description;
+    if (tags) userPathDetails.tags = tags;
+    if (likes) userPathDetails.likes = likes;
+    if (collections) userPathDetails.collections = collections;
+    if (mainPath) userPathDetails.mainPath = mainPath;
+    if (completed) userPathDetails.completed = completed;
+    const userPath = new UserPath(userPathDetails);
+    await userPath.save();
+    res.json(userPath);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(`Server error: ${err._message}`);
+  }
+});
 
 module.exports = router;
