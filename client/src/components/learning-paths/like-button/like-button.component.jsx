c7af6ff99b03d7348fa2e2c30b2e5bcddd8cd373
Tai N <ndhuutai1@gmail.com>
2020-07-06 17:45:28 -0700

    renamed/added folder for "like" component.

---
+++ client/src/components/learning-paths/like-button/like-button.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './like-button.style.css';
+
+const LikeButton = ({ id, loggedInUser }) => {
+  function likeButtonClick(eventId, userId) {
+    const payload = { id: userId, collectionId: id };
+
+    fetch(`/api/collections/like/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  return (
+    <button onClick={() => likeButtonClick(id, loggedInUser)} type="button" className="button-like">
+      <i className="far fa-thumbs-up" />
+    &nbsp; Like Collection
+    </button>
+  );
+};
+
+export default LikeButton;
