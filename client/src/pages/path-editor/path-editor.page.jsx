50650256000da362e426acf02cc6f82958202370
Tai N <ndhuutai1@gmail.com>
2020-07-06 17:54:00 -0700

    added pathName state to context and controlled input for pathName.

--- client/src/pages/path-editor/path-editor.page.jsx
+++ client/src/pages/path-editor/path-editor.page.jsx
@@ -1,6 +1,4 @@
-import React, {
-  useContext, useEffect, useState, useRef,
-} from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 
 import { useHistory, useLocation } from 'react-router-dom';
 import { motion, useMotionValue } from 'framer-motion';
@@ -10,6 +8,7 @@ import styles from './path-editor.style.css';
 
 import PathEditorContext from '../../contexts/path-editor-context';
 import { setCollections } from '../../actions/path-editor';
+import { setPathName } from '../../actions/path-editor';
 
 // --- reference only ---
 // const initialColors = [
@@ -39,9 +38,7 @@ const flat = {
   transition: { delay: 0.3 },
 };
 
-const Item = ({
-  setPosition, moveItem, i, name,
-}) => {
+const Item = ({ setPosition, moveItem, i, name }) => {
   const [isDragging, setDragging] = useState(false);
 
   // We'll use a `ref` to access the DOM element that the `motion.li` produces.
@@ -103,7 +100,9 @@ const PathEditor = ({ userInfo }) => {
   // const [colors, setColors] = useState(initialColors);
   const history = useHistory();
   const location = useLocation();
-  const { chosenCollections, dispatch } = useContext(PathEditorContext);
+  const { pathName, chosenCollections, dispatch } = useContext(PathEditorContext);
+  const [isOpen, setOpen] = useState(false);
+  const [pathNameInput, setPathNameInput] = useState('');
 
   // We need to collect an array of height and position data for all of this component's
   // `Item` children, so we can later us that in calculations to decide when a dragging
@@ -132,7 +131,8 @@ const PathEditor = ({ userInfo }) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        name: userInfo,
+        id: userInfo,
+        name: pathName,
         author: userInfo,
         description: 'We collectively agreed that we will ignore this :D',
         collections: chosenCollections.map((collection) => collection._id),
@@ -149,12 +149,29 @@ const PathEditor = ({ userInfo }) => {
       });
   };
 
+  const handlePathNameSave = () => {
+    dispatch(setPathName(pathNameInput));
+    setOpen(!isOpen);
+  };
+
   return (
     <div>
       <main className={styles.PathEditorPage}>
+        {isOpen ? (
+          <div>
+            <label htmlFor="path-name">
+              <input type="text" id="path-name" value={pathNameInput} onChange={(e) => setPathNameInput(e.target.value)} />
+              <button type="button" onClick={handlePathNameSave}>
+                Save
+              </button>
+            </label>
+          </div>
+        ) : (
+          <h3 onClick={() => setOpen(!isOpen)}>{pathName || '<Path Name>'}</h3>
+        )}
         <ul className={styles.PathEditor}>
-          {chosenCollections.length > 0
-            && chosenCollections.map(({ title }, i) => <Item key={title} i={i} name={title} setPosition={setPosition} moveItem={moveItem} />)}
+          {chosenCollections.length > 0 &&
+            chosenCollections.map(({ title }, i) => <Item key={title} i={i} name={title} setPosition={setPosition} moveItem={moveItem} />)}
         </ul>
         <button className={styles.AddCollectionButton} onClick={handleAdd} type="button">
           Add
