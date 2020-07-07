82e7c8b0aeea5ad6dbef8b293c32a807d70f73d1
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-07-06 22:40:24 -0400

    this commit added accordion stuff

--- client/src/pages/path-editor-add-collection/path-editor-add-collection.page.jsx
+++ client/src/pages/path-editor-add-collection/path-editor-add-collection.page.jsx
@@ -1,13 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { motion, AnimatePresence } from "framer-motion";
 import ExpandedCollectionView from '../../components/collection-expand-view/collection-expand-view.component';
 
 import styles from './path-editor-add-collection.style.css';
+
+const accordionIds = [0,1,2,3,4,5,6,7,8,9,10];
+
+const Accordion = ({ i, expanded, setExpanded }) => {
+  const isOpen = i === expanded;
+
+  // By using `AnimatePresence` to mount and unmount the contents, we can animate
+  // them in and out while also only rendering the contents of open accordions
+  return (
+    <div className={styles.CollectionItem}>
+      <motion.header
+        className={styles.CollectionHeader}
+        initial={false}
+        animate={{ backgroundColor: isOpen ? "#3F3D56" : "#3F3D56" }}
+        onClick={() => setExpanded(isOpen ? false : i)}
+      >
+        <h3>asdasda</h3>
+      </motion.header>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.section
+            key="content"
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 }
+            }}
+            transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+          >
+            <motion.div
+              transition={{ duration: 0.8 }}
+              className={styles.CollectionView}
+            >
+              <p>ayyyyyyy</p>
+            </motion.div>
+          </motion.section>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
 
 const AddCollectionView = () => {
   const history = useHistory();
 
   const [collectionsByCategory, setCollections] = useState(null);
+  const [expanded, setExpanded] = useState(0);
 
   const putCollectionsInCategories = (collectionsFromDb) => {
     const newCollections = collectionsFromDb.reduce((acc, curCollection) => {
@@ -75,7 +120,10 @@ const AddCollectionView = () => {
         : collections.map(({ title, category, description }) => (
             <ExpandedCollectionView key={title} title={title} category={category} description={description} />
           ))} */}
-      {collectionsRender}
+      {/* {collectionsRender} */}
+      {accordionIds.map(i => (
+        <Accordion i={i} expanded={expanded} setExpanded={setExpanded} />
+      ))}
       <div className={styles.AddCollectionButtons}>
         <button className={styles.CancelButton} type="button" onClick={handleCancel}>
           Cancel
